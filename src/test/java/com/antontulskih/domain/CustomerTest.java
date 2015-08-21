adc3f6f624b31115481d6e85ac30c738e192996b
Anton Tulskih <anton.tulskih@gmail.com>
2015-08-21 07:04:06 +0300

    Commented out tests on Customer setFirstName() setLastName() and setCarNumber() due to spring validation

--- src/test/java/com/antontulskih/domain/CustomerTest.java
+++ src/test/java/com/antontulskih/domain/CustomerTest.java
@@ -40,66 +40,69 @@ public final class CustomerTest {
         assertEquals("Customer ID", expected, actual);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetCustomerFirstName_NullString() throws Exception {
-            customer.setFirstName(null);
-    }
+//    Commented out because the validation in setters have been removed due
+// to spring validation.
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetCustomerFirstName_EmptyString() throws Exception {
-        customer.setFirstName("");
-    }
-
-    @Test
-    public void testSetCustomerFirstName_ValidString() throws Exception {
-        final String validString = "Michael";
-        final String expected = "Michael";
-        customer.setFirstName(validString);
-        final String actual = customer.getFirstName();
-        assertEquals("Customer first name", expected, actual);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetCustomerLastName_NullString() throws Exception {
-        customer.setLastName(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetCustomerLastName_EmptyString() throws Exception {
-        customer.setLastName("");
-    }
-
-    @Test
-    public void testSetCustomerLastName_ValidString() throws Exception {
-        final String validString = "Jackson";
-        final String expected = "Jackson";
-        customer.setLastName(validString);
-        final String actual = customer.getLastName();
-        assertEquals("Customer first name", expected, actual);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetCustomerCardNumber_NullString() throws Exception {
-        customer.setCardNumber(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetCustomerCardNumber_EmptyString() throws Exception {
-        customer.setCardNumber("");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetCustomerCardNumber_InvalidString() throws Exception {
-        customer.setCardNumber("FOO");
-    }
-
-    @Test
-    public void testSetCustomerCardNumber_ValidString() throws Exception {
-        final String validString = "0000111122223333";
-        final String expected = "0000111122223333";
-        customer.setCardNumber(validString);
-        final String actual = customer.getCardNumber();
-        assertEquals("Customer card number", expected, actual);
-    }
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSetCustomerFirstName_NullString() throws Exception {
+//            customer.setFirstName(null);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSetCustomerFirstName_EmptyString() throws Exception {
+//        customer.setFirstName("");
+//    }
+//
+//    @Test
+//    public void testSetCustomerFirstName_ValidString() throws Exception {
+//        final String validString = "Michael";
+//        final String expected = "Michael";
+//        customer.setFirstName(validString);
+//        final String actual = customer.getFirstName();
+//        assertEquals("Customer first name", expected, actual);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSetCustomerLastName_NullString() throws Exception {
+//        customer.setLastName(null);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSetCustomerLastName_EmptyString() throws Exception {
+//        customer.setLastName("");
+//    }
+//
+//    @Test
+//    public void testSetCustomerLastName_ValidString() throws Exception {
+//        final String validString = "Jackson";
+//        final String expected = "Jackson";
+//        customer.setLastName(validString);
+//        final String actual = customer.getLastName();
+//        assertEquals("Customer first name", expected, actual);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSetCustomerCardNumber_NullString() throws Exception {
+//        customer.setCardNumber(null);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSetCustomerCardNumber_EmptyString() throws Exception {
+//        customer.setCardNumber("");
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testSetCustomerCardNumber_InvalidString() throws Exception {
+//        customer.setCardNumber("FOO");
+//    }
+//
+//    @Test
+//    public void testSetCustomerCardNumber_ValidString() throws Exception {
+//        final String validString = "0000111122223333";
+//        final String expected = "0000111122223333";
+//        customer.setCardNumber(validString);
+//        final String actual = customer.getCardNumber();
+//        assertEquals("Customer card number", expected, actual);
+//    }
 
 }
